e96827360df6420f37fb6da9560df0e6cfdaecde
Priyapatil1612 <149194396+Priyapatil1612@users.noreply.github.com>
2024-06-09 20:43:22 -0400

    Time: 47 ms (12.55%) | Memory: 16.5 MB (77.64%) - LeetSync

---
+++ 1137-height-checker/height-checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected_li = sorted(heights)
+        incorrect_count = 0
+        for i in range(len(heights)):
+            if expected_li[i] == heights[i]:
+                continue
+            else:
+                incorrect_count+=1
+        return incorrect_count
