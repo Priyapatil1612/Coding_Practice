b8830facd4a2fdfc8ca42506ae0132ee2db0e1cf
Priyapatil1612 <149194396+Priyapatil1612@users.noreply.github.com>
2024-07-12 17:42:24 -0400

    Time: 302 ms (55.98%) | Memory: 18.4 MB (40.25%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,42 +1,19 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        def remove_and_score(s, target, points):
-            stack = []  # Initialize an empty stack
+        def give_score(s,target,points):
+            stack1 = []
             score = 0
-            for char in s:
-                # Check if the top of the stack and the current character form the target substring
-                if stack and stack[-1] + char == target:
-                    stack.pop()  # Remove the top element from the stack
-                    score += points  # Add points for the removal
+            for i in s:
+                if stack1 and stack1[-1]+i == target:
+                    stack1.pop()
+                    score += points
                 else:
-                    stack.append(char)  # Push the current character onto the stack
-            return ''.join(stack), score  # Return the modified string and the score
-    
-        if x > y:
-            # Remove "ab" first
-            s, score1 = remove_and_score(s, 'ab', x)
-            _, score2 = remove_and_score(s, 'ba', y)
+                    stack1.append(i)
+            return ''.join(stack1),score
+        if x > y :
+            s,score1 = give_score(s,'ab',x)
+            _,score2 = give_score(s,'ba',y)
         else:
-            # Remove "ba" first
-            s, score1 = remove_and_score(s, 'ba', y)
-            _, score2 = remove_and_score(s, 'ab', x)
-        
-        return score1 + score2
-
-        # def give_score(s,target,points):
-        #     stack1 = []
-        #     score = 0
-        #     for i in s:
-        #         if stack1 and stack1[-1]+i == target:
-        #             stack1.pop()
-        #             score += points
-        #         else:
-        #             stack1.append(i)
-        #     return score
-        # if x > y :
-        #     score1 = give_score(s,'ab',x)
-        #     score2 = give_score(s,'ba',y)
-        # else:
-        #     score1 = give_score(s,'ba',y)
-        #     score2 = give_score(s,'ab',x)
-        # return score1+score2
+            s,score1 = give_score(s,'ba',y)
+            _,score2 = give_score(s,'ab',x)
+        return score1+score2
