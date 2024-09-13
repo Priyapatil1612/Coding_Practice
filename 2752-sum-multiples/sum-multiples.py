4108d198a77bceaada96810d78d39920ee0b1930
Priyapatil1612 <149194396+Priyapatil1612@users.noreply.github.com>
2024-09-13 00:20:59 -0400

    Time: 61 ms (61.92%) | Memory: 16.4 MB (82.52%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -3,6 +3,5 @@ class Solution:
         temp_sum = 0
         for i in range(1,n+1):
             if i%3 == 0 or i%5==0 or i%7 == 0:
-                print(i)
                 temp_sum += i
         return temp_sum
