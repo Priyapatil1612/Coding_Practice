6bd2c652be60b8f2fa9225a8759b361008a2c2b8
Priyapatil1612 <149194396+Priyapatil1612@users.noreply.github.com>
2025-03-05 16:35:03 -0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (19.15%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -3,15 +3,10 @@ class Solution:
         my_dict = {}
         for i in nums1,nums2:
             for j in i:
-                key = j[0]
-                value = j[1]
-                if key in my_dict:
-                    my_dict[key] += value
+                if j[0] in my_dict:
+                    my_dict[j[0]] += j[1]
                 else:
-                    my_dict[key] = value
+                    my_dict[j[0]] = j[1]
         sorted_dict = dict(sorted(my_dict.items()))
-        ans = []
-        for k,v in sorted_dict.items():
-            temp_li = [k,v]
-            ans.append(temp_li)
+        ans = [[k, v] for k, v in sorted_dict.items()]
         return ans
