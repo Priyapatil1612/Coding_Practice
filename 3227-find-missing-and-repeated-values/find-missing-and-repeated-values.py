761764578e8a3c32e443ed665c4dd73adad56c8b
Priyapatil1612 <149194396+Priyapatil1612@users.noreply.github.com>
2025-03-06 15:07:34 -0500

    Time: 3 ms (94.12%) | Memory: 18.1 MB (64.09%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,19 +1,13 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-        temp_list = []
+        temp_list = {}
         for i in grid:
             for j in i:
-                temp_list.append(j)
-        hash_cnt = Counter(temp_list)
-        # for k in hash_cnt.keys():
-        #     if hash_cnt[k] > 1:
-        #         rep_item = k
-        # avail_items = list(hash_cnt.keys())
-        # abs_item = None
+                temp_list[j] = temp_list.get(j,0)+1
         for i in range(1,n*n+1):
-            if i not in hash_cnt:
+            if i not in temp_list:
                 abs_item = i
-            elif hash_cnt[i] == 2:
+            elif temp_list[i] == 2:
                 rep_item = i
         return [rep_item,abs_item]
