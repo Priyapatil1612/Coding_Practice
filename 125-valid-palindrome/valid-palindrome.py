31ea7e65d027507c28099a746de0dc6c43800a91
Priya Patil <ppatil22@buffalo.edu>
2025-08-12 11:06:57 -0400

    Time: 11 ms (35.05%) | Memory: 23.2 MB (17.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,36 +1,29 @@
 class Solution:
     import string
     def isPalindrome(self, s: str) -> bool:
-        # str_l = s.lower()
-        # str_l = str_l.replace(" ","")
-        # translator = str.maketrans('','',string.punctuation)
-        # final_s = str_l.translate(translator)
-        # left = 0
-        # right = len(final_s)-1
-        # while left < right:
-        #     if final_s[left] != final_s[right]:
-        #         return False
-        #     left += 1
-        #     right -= 1
-        # return True
+        ## Two POINTER APPROACH -----> O(n^2)
 
-
-
-
-        lower_s = s.lower()
-        new_s = ""
-        for char in lower_s:
-            if char.isalnum():
-                new_s += char
+        # lower_s = s.lower()
+        # new_s = ""
+        # for char in lower_s:
+        #     if char.isalnum():
+        #         new_s += char
+        #         i = 0
+        #         j = len(new_s) - 1
+        #         while i <= j:
+        #             if new_s[i] != new_s[j]:
+        #                 return False
+        #             i += 1
+        #             j -= 1
+        #         return True
         
-        i = 0
-        j = len(new_s) - 1
-        while i <= j:
-            if new_s[i] != new_s[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
+        ## REVERSE STRING  ------------>O(n)
+        new_s = ''.join(c.lower() for c in s if c.isalnum())
+        if new_s == new_s[::-1]:
+            return True
+        return False
+
+
         
 
 
